e48cd584b274874c9f6a3d173bdaa675d59d85b7
Kat Siegal <kat@mobiata.com>
2015-07-25 16:30:00 -0500

    Setting up UI file structure

---
+++ SetsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SetsViewController.swift
+//  Bassdrive
+//
+//  Created by Katherine Siegal on 7/25/15.
+//  Copyright (c) 2015 Richard Sbresny. All rights reserved.
+//
+
+import Foundation
