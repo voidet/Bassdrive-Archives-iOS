e4dcc7e7a124a0a0dd46cdbe8123e8d5f4984378
Richard Sbresny <rsbresny@expedia.com>
2015-07-26 06:49:21 -0700

    About to add in download manager

---
+++ Bassdrive/BassdriveSet.swift
@@ -0,0 +1,13 @@
+//
+//  BassdriveSet.swift
+//  Bassdrive
+//
+//  Created by Richard Sbresny on 7/26/15.
+//  Copyright (c) 2015 Richard Sbresny. All rights reserved.
+//
+
+import UIKit
+
+class BassdriveSet: NSObject {
+   
+}
