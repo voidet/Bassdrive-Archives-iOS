25ca421414c75e81a5d12c5597043675b84eb346
Richard Sbresny <rsbresny@expedia.com>
2015-07-25 19:34:07 -0500

    Working on circular referenced storyboards

---
+++ Bassdrive/RSSetTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  RSSetTableViewCell.swift
+//  Bassdrive
+//
+//  Created by Richard Sbresny on 7/25/15.
+//  Copyright (c) 2015 Richard Sbresny. All rights reserved.
+//
+
+import UIKit
+
+class RSSetTableViewCell: UITableViewCell {
+
+    @IBOutlet var bassdriveSetTitleLabel:UILabel!
+    @IBOutlet var bassdriveSetImageView:UIImageView!
+
+}
--- SetsViewController.swift
+++ SetsViewController.swift
@@ -7,30 +7,56 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class SetsViewController: UIViewController, UITableViewDataSource {
+class SetsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var sets:[AnyObject] = []
-    
-//    var data = [Int: string]()
-//    
-//    data[1] = "link 1"
-//    data[2] = "link 2"
-//    data[3] = "link 3"
-    
+    var sets:JSON?
     var data = [1 : "link 1", 2 : "link 2", 3 : "link 3"]
+    let requestURL:String = "http://localhost:8080/parse.php"
     
     @IBOutlet var tableView:UITableView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Alamofire.request(.GET, requestURL).responseJSON { _, _, responseJSON, _ in
+            self.sets = JSON(responseJSON!)
+            self.tableView.reloadData()
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "goToSetList") {
+            let destVC:SetsViewController = segue.destinationViewController as! SetsViewController
+            let indexPath:NSIndexPath = self.tableView.indexPathForSelectedRow()!
+
+            var key:String? = self.sets?.dictionaryObject?.keys.array[indexPath.row] ?? ""
+            println(key)
+            destVC.sets = self.sets![key!]
+        }
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return data.count
+        return self.sets?.count ?? 0
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
-        var cell:UITableViewCell = UITableViewCell()
-        cell.textLabel?.text = data[indexPath.row]
+        var cell:RSSetTableViewCell = tableView.dequeueReusableCellWithIdentifier("setTitleCell") as! RSSetTableViewCell
+        let key:String = self.sets?.dictionaryObject?.keys.array[indexPath.row] ?? ""
+        cell.bassdriveSetTitleLabel.text = key
+        
+        cell.userInteractionEnabled = true
+        if (self.sets![key].dictionary?.values != nil) {
+            cell.userInteractionEnabled = false
+        }
+        
         return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
     }
 }
