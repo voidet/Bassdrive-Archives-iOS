67d962afadae050fd1263013ff0f3247ba65befb
Richard Sbresny <rsbresny@expedia.com>
2015-07-25 20:18:49 -0500

    Added in tasty managers

---
+++ Bassdrive/DownloadTask.swift
@@ -0,0 +1,15 @@
+//
+//  DownloadTask.swift
+//  Bassdrive
+//
+//  Created by Richard Sbresny on 7/25/15.
+//  Copyright (c) 2015 Richard Sbresny. All rights reserved.
+//
+
+import UIKit
+
+struct DownloadTask {
+    
+
+   
+}
---
+++ Bassdrive/RSDownloadManager.swift
@@ -0,0 +1,19 @@
+//
+//  RSDownloadManager.swift
+//  Bassdrive
+//
+//  Created by Richard Sbresny on 7/25/15.
+//  Copyright (c) 2015 Richard Sbresny. All rights reserved.
+//
+
+import UIKit
+
+class RSDownloadManager {
+    
+    var downloadQueue:[DownloadTask] = []
+    
+    func enqueForDownload(requestURL:String) {
+    
+    }
+   
+}
